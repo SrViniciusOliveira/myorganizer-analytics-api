99febfa3b723c34b415f292c0c537b059bba0ed4
SrViniciusOliveira <viniciusgabriel340@yahoo.com>
2026-05-28 16:34:15 -0300

    aprimoração e implementação de metodos de segurança e tratamento de api

--- app.py
+++ app.py
@@ -1,22 +1,44 @@
+from flask import request
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from dotenv import load_dotenv
 from flask import Flask, Response
 from flask_cors import CORS
 import sqlite3
+import re
 import os
+
+
 
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={
+    r"/*": {
+        "origins": [
+            "http://localhost:5173"
+        ]
+    }
+})
+
+
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits = ["100 per hour"]
+)
 
 # Banco SQLite
 conn = sqlite3.connect("database.db", check_same_thread=False)
+conn.execute("PRAGMA journal_mode=WAL;")
 cursor = conn.cursor()
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS visualizador_projetos (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome_projeto TEXT
+    nome_projeto TEXT,
+    ip TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
 """)
 
@@ -36,6 +58,12 @@ def home():
 @app.route("/analytics/<project>", methods=["GET"])
 def get_visualizacoes(project):
 
+    if len(project) > 50:
+        return {"error": "nome invalido"}, 400 
+    
+    if not re.match(r'^[a-zA-Z0-9_-]+$', project):
+        return {"error": "nome invalido"}, 400
+
     cursor = conn.cursor()
 
     cursor.execute("""
@@ -53,14 +81,44 @@ def get_visualizacoes(project):
 
 # POST - registrar view
 @app.route("/visualizacao/<project>", methods=["POST"])
+@limiter.limit("10 per minute")
 def registrar_visualizacao(project):
+
+    if len(project) > 50:
+        return {"error": "nome invalido"}, 400 
+    
+    if not re.match(r'^[a-zA-Z0-9_-]+$', project):
+        return {"error": "nome invalido"}, 400
 
     cursor = conn.cursor()
 
+    # Pega IP Do Usuário
+    ip = request.headers.get("X-Forwarded-For") or request.remote_addr or "unknown"
+
+    if "," in ip:
+        ip = ip.split(",")[0].strip()
+
+
+    # Verifica se já visualizou nas últimas 24 Horas
     cursor.execute("""
-        INSERT INTO visualizador_projetos(nome_projeto)
-        VALUES (?)
-    """, (project,))
+    SELECT id FROM visualizador_projetos
+    WHERE nome_projeto = ?
+    AND ip = ?
+    AND datetime(created_at) > datetime('now', '-24 hours')
+    """, (project, ip))
+
+    exists  = cursor.fetchone()
+
+    # Se já existe -> Não conta novamente 
+    if exists:
+        cursor.close()
+        return {"message": "visualizacao já registrada"}
+    
+    # Registra nova visualizacao
+
+    cursor.execute("""
+                   INSERT INTO visualizador_projetos(nome_projeto, ip)
+                   VALUES (?, ?)""", (project, ip))
 
     conn.commit()
 
@@ -71,19 +129,18 @@ def registrar_visualizacao(project):
 
 # 🟢 BADGE PARA GITHUB
 @app.route("/badge/<project>")
+@limiter.limit("30 per minute")
 def badge(project):
+
+    if len(project) > 50:
+        return {"error": "nome invalido"}, 400 
+    
+    if not re.match(r'^[a-zA-Z0-9_-]+$', project):
+        return {"error": "nome invalido"}, 400
 
     cursor = conn.cursor()
 
-    # REGISTRA A VISUALIZAÇÃO
-    cursor.execute("""
-        INSERT INTO visualizador_projetos(nome_projeto)
-        VALUES (?)
-    """, (project,))
-
-    conn.commit()
-
-    # CONTA AS VISUALIZAÇÕES
+    # APENAS CONTA AS VISUALIZAÇÕES
     cursor.execute("""
         SELECT COUNT(*)
         FROM visualizador_projetos
@@ -102,14 +159,12 @@ def badge(project):
 </svg>"""
 
     return Response(
-    svg,  
-    mimetype="image/svg+xml",
-    headers={
-        "Cache-Control": "no-cache, no-store, must-revalidate",
-        "Pragma": "no-cache",
-        "Expires": "0"
-    }
-)
+        svg,
+        mimetype="image/svg+xml",
+        headers={
+            "Cache-Control": "public, max-age=300"
+        }
+    )
 
 # RUN
 if __name__ == "__main__":
